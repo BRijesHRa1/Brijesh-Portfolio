e09915531a090c02c65c5b3f2ff62e127f066976
Brijesh Rai <brijeshrai@Mac.lan>
2025-02-28 21:36:46 -0600

    Fix: Updated image paths and references

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/Brijesh_picture.png"
+        src="/images/Brijesh_picture.png"
         alt="Brijesh"
         className="max-h-[450px] w-auto"
       />
